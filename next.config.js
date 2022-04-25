41bc6930ea85bf886a9f8e9f24e3d0bb876aff50
duygu.kizanlik <duygu.kizanlik@bulutmd.com>
2022-04-25 22:55:04 +0300

    next.config edited

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  env: {
+    apiKey: process.env.API_KEY,
+  },
+  //Yalnızca sunucuya yönelik herhangi bir çalışma zamanı yapılandırması için
+  serverRuntimeConfig: {
+    apiKey: process.env.API_KEY,
+  },
+  //Hem istemci hem de sunucu tarafı koduna erişilebilen her şey için
   publicRuntimeConfig: {
     apiKey: process.env.API_KEY,
   },
