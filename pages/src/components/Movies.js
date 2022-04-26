f94637294e7c825e7865fd7601c699552b5fd70b
duygu.kizanlik <duygu.kizanlik@bulutmd.com>
2022-04-27 02:27:06 +0300

    movie upcoming api added and movies component edited

--- pages/src/components/Movies.js
+++ pages/src/components/Movies.js
@@ -1,44 +1,28 @@
 import React, { useState } from "react";
-import { ResultCard } from "./ResultCard";
-import getConfig from "next/config";
 import styles from "../../../styles/Home.module.css";
 
-export default function Movies() {
-  const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
-  const [query, setQuery] = useState("");
-  const [result, setResult] = useState([]); // api sonuçlarını depolamak için
-
-  const onChange = async (e) => {
-    e.preventDefault(); //tıklandığında bu eylemin engellenmesi gerekiyor ise kullanılır
-    setQuery(e.target.value); // Girdinin değerini alır ne eklenirse event.target.value aracılığıyla erişilir
-    let movies = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${publicRuntimeConfig.apiKey}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
-    ); // TMDB'den alınan movie API'si tanımlandı
-    movies = await movies.json(); // içerik çıkarmak için json kullanıldı
-    setResult(movies.results); // apiden çıkan veriler başta boş olan setResult'a set edildi.
-  };
-
+export default function Movies({
+  title,
+  index,
+  overview,
+  poster_path,
+  release_date,
+  vote_average,
+}) {
+  const IMAGES_API = "https://image.tmdb.org/t/p/w500/";
   return (
-    <div>
-      <div>
-        <input
-          className={styles.search}
-          type="text"
-          placeholder="Arama"
-          name="search"
-          value={query}
-          onChange={onChange}
-        />
+    <div className={styles.moviesPage} key={index}>
+      <div className={styles.h3}>
+        <h3>
+          {title} - {vote_average}
+        </h3>
       </div>
-      {result?.length > 0 && (
-        <div className={styles.results}>
-          {result?.map((movie) => (
-            <ul>
-              <ResultCard movie={movie} />
-            </ul>
-          ))}
-        </div>
-      )}
+
+      <img src={IMAGES_API + poster_path} alt={title}></img>
+      <div className={styles.overview}>
+        {overview}
+        <h4>{release_date ? release_date.substring(0, 4) : "-"} </h4>
+      </div>
     </div>
   );
 }
